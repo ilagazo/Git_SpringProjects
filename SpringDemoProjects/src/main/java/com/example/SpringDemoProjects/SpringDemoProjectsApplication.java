9168806304b1cd1c544844c79f58c1b1bcf3d5ca
ivanro_lagazo <ivanro.lagazo@iconfitness.com>
2021-07-01 16:38:37 -0600

    Starting 1st Spring Demo Project

---
+++ SpringDemoProjects/src/main/java/com/example/SpringDemoProjects/SpringDemoProjectsApplication.java
@@ -0,0 +1,14 @@
+package com.example.SpringDemoProjects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RestController
+public class SpringDemoProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoProjectsApplication.class, args);
+	}
+
+}
---
+++ SpringDemoProjects/src/test/java/com/example/SpringDemoProjects/SpringDemoProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringDemoProjects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
