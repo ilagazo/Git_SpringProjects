a62862869d74941ac467e05b5a869fc7bb31fa1d
ivanro_lagazo <ivanro.lagazo@iconfitness.com>
2021-07-01 17:01:21 -0600

    Function added for Hello World

--- SpringDemoProjects/src/main/java/com/example/SpringDemoProjects/SpringDemoProjectsApplication.java
+++ SpringDemoProjects/src/main/java/com/example/SpringDemoProjects/SpringDemoProjectsApplication.java
@@ -3,6 +3,8 @@ package com.example.SpringDemoProjects;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// This project is the Hello World Project
+
 @SpringBootApplication
 @RestController
 public class SpringDemoProjectsApplication {
@@ -11,4 +13,8 @@ public class SpringDemoProjectsApplication {
 		SpringApplication.run(SpringDemoProjectsApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
 }
